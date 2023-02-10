45d1bec9a8b33480ffd0271b206dcad18011a633
Evgen <jenpl1451@gmail.com>
2023-02-10 09:59:59 +0200

    addBootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Button variant="primary" onClick={() => alert('click')}>Alert</Button>
     </div>
   );
 }
