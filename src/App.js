84ac3ed0a907c07154d569d980d5058adf2a2ad3
Evgen <jenpl1451@gmail.com>
2023-02-10 11:37:57 +0200

    Add Header Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.scss';
 import {RouterProvider} from "react-router-dom";
 import {router} from './Router';
+import {Header} from './components/header/Header';
+import {Footer} from './components/footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <RouterProvider router={router} />
+      <Footer />
     </div>
   );
 }
