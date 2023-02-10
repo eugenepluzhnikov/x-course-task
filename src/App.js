953fc40c5faf1a837721a420e0a151b99fc4c6be
Evgen <jenpl1451@gmail.com>
2023-02-10 12:11:42 +0200

    css header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Header} from './components/header/Header';
 import {Footer} from './components/footer/Footer';
 
 
-function App() {
+const App = () => {
   return (
     <div className="app">
       <Header />
