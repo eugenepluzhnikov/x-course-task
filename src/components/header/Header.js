adf1ec649d520dc6888c11b4b10a09504871913a
Evgen <jenpl1451@gmail.com>
2023-02-13 12:44:55 +0200

    header _css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,27 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import {Button, Nav, Image, Navbar, Container} from 'react-bootstrap';
+
+import "./Header.scss";
+
+import cartImg from "../../img/cart.svg";
+import avatarImg from "../../img/avatar.svg"
 
 export const Header = () => {
   return (
-    <Navbar>
-      <Container>
-        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+    <Navbar className='navbar'>
+      <Container className='container'>
+        <Navbar.Brand href="#home">JS BAND STORE / Your full name</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text>
+          <Nav>
+            <Image src={cartImg} alt="shopping cart icon"  className="cart-img" rounded />
+            <Button variant="outline-dark" >Sign-Out</Button>
+            <div className='avatar'>
+            <Image src={avatarImg} alt="avatar" rounded />
+            </div>
+            <Navbar.Text className='navbar-text'>Username</Navbar.Text>
+          </Nav>
+
+          
         </Navbar.Collapse>
       </Container>
     </Navbar>
