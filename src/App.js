eb9f0dc8f6cb8a0f5b3f25fd8c9d8d345535dd7a
Evgen <jenpl1451@gmail.com>
2023-02-13 18:25:44 +0200

    footer_scss

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ const App = () => {
   return (
     <div className="app">
       <Header />
-      <div className="page">
+      <section className="page">
         <RouterProvider router={router} />
-      </div>
+      </section>
       <Footer />
     </div>
   );
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,8 +3,8 @@ import "./Footer.scss";
 
 export const Footer = () => {
   return (
-    <section className='footer'>
+    <footer className='footer'>
       “Виконано в<a className='footer-link' href="https://prometheus.org.ua/">Prometheus</a>© 2022”
-    </section>
+    </footer>
   );
 }
