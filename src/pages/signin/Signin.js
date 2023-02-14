7ff470ba7f2d683db087b87f83d9879d178fe78d
Evgen <jenpl1451@gmail.com>
2023-02-14 09:57:11 +0200

    signin and scss

--- src/pages/signin/Signin.js
+++ src/pages/signin/Signin.js
@@ -1,7 +1,18 @@
+import { Image } from "react-bootstrap";
+
+import avatar2 from "../../img/avatar2.svg";
+
+import "./Signin.scss";
+
 export const Signin = () => {
   return (
-    <div>
-      Signin
-    </div>
-  )
-}
+    <main class="signin">
+      <Image src={avatar2} alt="avatar" className="avatar2" rounded />
+
+      <p class="h4">Username</p>
+      <form name="full-name" action="#">
+        <input type="text" name="username" id="name" placeholder="type Username"/>
+        <input type="submit" id="button" value="Sign-in"/>
+      </form>
+    </main>
+  )}
