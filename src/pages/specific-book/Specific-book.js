cc7d66951ce730ffdab5058f290c03da94344478
Evgen <jenpl1451@gmail.com>
2023-02-15 15:06:27 +0200

    book info

--- src/pages/specific-book/Specific-book.js
+++ src/pages/specific-book/Specific-book.js
@@ -1,4 +1,4 @@
-import { Image, Button } from "react-bootstrap";
+import { Image, Button, Container, Row, Col } from "react-bootstrap";
 
 import "./Specific-book.scss";
 
@@ -8,51 +8,57 @@ import arrowDown from "../../img/arrowDown.svg";
 
 export const SpecificBook = () => {
   return (
-    <main class="specificBook">
-      <section class="img">
-        <Image src={book} alt="'Енеїда' Іван Котляревський" className="book" />
-      </section>
+    <Container className="specific-book">
+      <Row>
+        <Col md={5}>
+          <Image src={book} alt="'Енеїда' Іван Котляревський" className="img-book" />
+        </Col>
 
-      <section class="bookName">
-        <div className="title">
-          Book name:<span className="name">Енеїда</span>
-        </div>
-        <div className="title">
-          Book author:<span className="name">Іван Котляревський</span>
-        </div>
-        <div className="title">
-          Book level:<span className="name">Beginner</span>
-        </div>
-        <div className="title">
-          Book name:<span className="name">core</span>
-        </div>
-      </section>
+        <Col md={4}>
+          <Row className="info">
+            <Col className="title">Book name:</Col>
+            <Col>Енеїда</Col>
+          </Row>
+          <Row className="info">
+            <Col className="title">Book author:</Col>
+            <Col>Іван Котляревський</Col>
+          </Row>
+          <Row className="info">
+            <Col className="title">Book level:</Col>
+            <Col>Beginner</Col>
+          </Row>
+          <Row className="info">
+            <Col className="title">Book name:</Col>
+            <Col>core</Col>
+          </Row>
+        </Col>
 
-      <section className="prise">
-        <div className="priceTitle">
-          Price, $
-          <div>0</div>
-        </div>
-        <div className="priceTitle">
-          Count
-          <div className="countButton">
-            <Button variant="outline-dark">Card</Button>
-            <div className="countButtonArrow">
-              <Button variant="outline-dark" className="buttonArrow">
-                <img src={arrowUp} alt="arrowUp" className="imgArrow" />
-              </Button>
-              <Button variant="outline-dark" className="buttonArrow ">
-                <img src={arrowDown} alt="arrowDown" className="imgArrow" />
-              </Button>
+        <Col md={3}>
+          <div className="priceTitle">
+            Price, $
+            <div>0</div>
+          </div>
+          <div className="priceTitle">
+            Count
+            <div className="countButton">
+              <Button variant="outline-dark">Card</Button>
+              <div className="countButtonArrow">
+                <Button variant="outline-dark" className="buttonArrow">
+                  <img src={arrowUp} alt="arrowUp" className="imgArrow" />
+                </Button>
+                <Button variant="outline-dark" className="buttonArrow ">
+                  <img src={arrowDown} alt="arrowDown" className="imgArrow" />
+                </Button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="priceTitle">
-          Total price, $
-          <div>0</div>
-        </div>
-        <Button variant="outline-dark" className="addCard">Add to card</Button>
-      </section>
-
-    </main>
-  )}
+          <div className="priceTitle">
+            Total price, $
+            <div>0</div>
+          </div>
+          <Button variant="outline-dark" className="addCard">Add to card</Button>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
