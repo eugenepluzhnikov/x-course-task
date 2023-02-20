d9273c0af59fec1399c4f6263d442868fda8079e
Evgen <jenpl1451@gmail.com>
2023-02-20 15:28:41 +0200

    books search

--- src/pages/book-list/Book-list.js
+++ src/pages/book-list/Book-list.js
@@ -1,28 +1,36 @@
-import {Container, Row, Col, Form } from "react-bootstrap";
+import { Container, Row, Col, Form } from "react-bootstrap";
+import { useState, useMemo } from "react";
 
 import "./Book-list.scss";
 
 import { Book } from "./book/Book";
 import data from "../../fixtures/books.json"
 
-
-
 export const BookList = () => {
-
+  const [search, setSearch] = useState("");
+  const books = useMemo(() => {
+    if (!search) {
+      return (data.books);
+    }
+    return data.books.filter((book) => {
+      return book.title.includes(search) || book.author.includes(search);
+    })
+  }, [search])
   return (
     <Container>
       <Row className="form-book-list">
-        <Col md={7}>
-          <Form.Control type="number" placeholder="Search my book name" />
-        </Col>
         <Col md={5}>
+          <Form.Control value={search} onChange={(e) => setSearch(e.target.value.trim())}
+            placeholder="Search my book name" />
+        </Col>
+        <Col md={4}>
           <Form.Select disabled>
             <option>Price</option>
           </Form.Select>
         </Col>
       </Row>
       <Row className="book" >
-        {data.books.map((book) => {
+        {books.map((book) => {
           return <Book key={book.id} {...book} />
         })
         }
