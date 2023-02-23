a794f1cdabb00a504d3c62f7469a665db9bb4397
Evgen <jenpl1451@gmail.com>
2023-02-23 13:16:42 +0200

    book-prise

--- src/pages/cart/Book-price/Book-price.js
+++ src/pages/cart/Book-price/Book-price.js
@@ -1,19 +1,42 @@
-import {Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
+
+
+import { useContext } from "react";
+import { CartContext } from "../../../context/context";
+
 
 import "./Book-prise.scss";
 
+import data from "../../../fixtures/books.json"
+
 export const BookPrice = () => {
-  return(
-    <Row className="book-price">
-        <Row className="book-price-line">
-          <Col md={3}>Book name</Col>
-          <Col md={3}>Count</Col>
-          <Col md={6} className="total-price" >Total price</Col>
-        </Row>
-        <Row className="price">
-          <Col  md={3} className="price-text">Total price, $</Col>
-          <Col md={1} >100</Col>
-        </Row>
+
+  const [cart] = useContext(CartContext);
+  console.log(cart);
+  const id = cart[0].id - 1;
+  const book = data.books[id];
+  console.log(book);
+
+  return (
+
+
+    <>
+      <Row >
+        <Col className="cart-bt-col">
+        <Button variant="outline-dark" className="cart-bt" >Purchase</Button>
+        </Col>
       </Row>
+      <Row className="book-price-line">
+        <Col md={3}>{book.title}</Col>
+        <Col md={3}>{book.author}</Col>
+        <Col md={6} className="total-price" >{book.price}</Col>
+      </Row>
+      <Row className="price">
+        <Col md={3} className="price-text">Total price, $</Col>
+        <Col md={1} >100</Col>
+      </Row>
+    </>
+
+
   )
 }
--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,31 +1,19 @@
 import { useContext } from 'react';
 import { CartContext } from '../../context/context';
 
-
-
-
-
-import {Button, Container, Row } from "react-bootstrap";
+import {Container} from "react-bootstrap";
 
 import { BookPrice } from "./Book-price/Book-price";
 import { CartEmpty } from "./Cart-empty/Cart-empty"
 
 import "./Cart.scss";
 
-
 export const Cart = () => {
 
   const [cart] = useContext(CartContext);
-  console.log(cart);
-
-
   return (
     <Container className="cart">
-      <Row className="cart-bt">
-        <Button variant="outline-dark">Purchase</Button>
-      </Row>
-      { cart.length === 0 && <CartEmpty />}
-      { cart.length !== 0 && <BookPrice />}
+      { cart.length === 0 ? <CartEmpty /> :<BookPrice /> }
     </Container>
   )
 }
