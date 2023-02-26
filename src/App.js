b06104556c87737c1190131cb325a6d9aafcc03a
Evgen <jenpl1451@gmail.com>
2023-02-26 11:19:36 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.scss';
 import { Router } from './Router';
 import { Header } from './components/header/Header';
 import { Footer } from './components/footer/Footer';
-import { UserContext, CartContext} from './context/context';
-import {LOCAL_STORAGE} from './App.constants'
+import { UserContext, CartContext } from './context/context';
+import { LOCAL_STORAGE } from './App.constants';
 
 const App = () => {
-  const [user, setUser] = useState(localStorage.getItem(LOCAL_STORAGE.USER) || null);
-  const localStorageCart = localStorage.getItem(LOCAL_STORAGE.CART)
-  const [cart, setCart] = useState(localStorageCart ? JSON.parse(localStorageCart) : []);
+  const [user, setUser] = useState(
+    localStorage.getItem(LOCAL_STORAGE.USER) || null
+  );
+  const localStorageCart = localStorage.getItem(LOCAL_STORAGE.CART);
+  const [cart, setCart] = useState(
+    localStorageCart ? JSON.parse(localStorageCart) : []
+  );
 
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE.CART, JSON.stringify(cart))
-  }, [cart])
+    localStorage.setItem(LOCAL_STORAGE.CART, JSON.stringify(cart));
+  }, [cart]);
   return (
     <main className="app">
       <UserContext.Provider value={[user, setUser]}>
@@ -23,9 +27,8 @@ const App = () => {
         </CartContext.Provider>
       </UserContext.Provider>
       <Footer />
-
     </main>
   );
-}
+};
 
 export default App;
