1a4bcf21507218c284389e65bf05ee00b5310977
Evgen <jenpl1451@gmail.com>
2023-02-26 11:20:53 +0200

    book price fix

--- src/pages/cart/Book-price/Book-price.js
+++ src/pages/cart/Book-price/Book-price.js
@@ -1,15 +1,14 @@
-import { Row, Col, Button } from "react-bootstrap";
+import { Row, Col, Button } from 'react-bootstrap';
 
-import {useNavigate } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
 
+import { useContext, useMemo } from 'react';
+import { CartContext } from '../../../context/context';
 
-import { useContext, useMemo } from "react";
-import { CartContext } from "../../../context/context";
+import { CartBook } from './cart-book/Cart-book';
+import data from '../../../fixtures/books.json';
 
-import { CartBook } from "./cart-book/Cart-book";
-import data from "../../../fixtures/books.json"
-
-import "./Book-price.scss";
+import './Book-price.scss';
 
 export const BookPrice = () => {
   const navigate = useNavigate();
@@ -17,24 +16,35 @@ export const BookPrice = () => {
   const [cart, setCart] = useContext(CartContext);
   const totalPrice = useMemo(() => {
     return cart.reduce((acc, item) => {
-      const { price } = data.books.find((b) => b.id === item.id)
+      const { price } = data.books.find((b) => b.id === item.id);
       return acc + price * item.amount;
-    }, 0)
-  }, [cart])
+    }, 0);
+  }, [cart]);
 
-  const handleClearCart = () => {
-    setCart([])
-  }
+  const handleClearCart = () => setCart([]);
+
   return (
     <>
-      <Row >
+      <Row>
         <Col className="cart-bt-col">
-          <Button variant="outline-dark" className="cart-bt" onClick={() => navigate('/books')} >Purchase</Button>
-          <Button variant="outline-dark" className="cart-bt" onClick={handleClearCart} >Clear Cart</Button>
+          <Button
+            variant="outline-dark"
+            className="cart-bt"
+            onClick={() => navigate('/books')}
+          >
+            Purchase
+          </Button>
+          <Button
+            variant="outline-dark"
+            className="cart-bt"
+            onClick={handleClearCart}
+          >
+            Clear Cart
+          </Button>
         </Col>
       </Row>
       {cart.map((item) => {
-        return <CartBook key={item.id} {...item} />
+        return <CartBook key={item.id} {...item} />;
       })}
       <Row className="price">
         <Col md={3}>Total price: </Col>
@@ -42,7 +52,4 @@ export const BookPrice = () => {
       </Row>
     </>
   );
-}
-
-
-
+};
